157fc7ff3ebecd467463c4c2c31a4e07563284b6
frankie <jsynthlib.org@terrorise.me.uk>
2011-09-23 08:21:45 +0000

    same change to Scenes as to Libraries

--- core/SceneFrame.java
+++ core/SceneFrame.java
@@ -478,12 +478,14 @@ class SceneFrame extends AbstractLibraryFrame {
 
         // begin PatchTableModel interface methods
         // It is caller's responsibility to update Table.
-        void addPatch(IPatch p) {
+        int addPatch(IPatch p) {
             list.add(new Scene(p));
+            return list.size() -1;
         }
 
-        void addPatch(IPatch p, int bankNum, int patchNum) { // wirski@op.pl
+        int addPatch(IPatch p, int bankNum, int patchNum) { // wirski@op.pl
             list.add(new Scene(p, bankNum, patchNum));
+            return list.size() -1;
         }
 
         void setPatchAt(IPatch p, int row, int bankNum, int patchNum) { // wirski@op.pl
