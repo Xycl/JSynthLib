f5db2f7ff4ffb410727d01997206a2d0929ddba8
pascalc <packe01@yahoo.se>
2014-10-14 11:20:31 +0000

    Fixes for JavaFX 8 upgrade - Knob control Fix for macosx profile activation in pom

--- src/main/java/org/jsynthlib/device/view/Knob.java
+++ src/main/java/org/jsynthlib/device/view/Knob.java
@@ -1,10 +1,134 @@
 package org.jsynthlib.device.view;
 
+import java.text.DecimalFormat;
+
+import javafx.event.EventHandler;
 import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 
 public class Knob extends Slider {
 
+    private static final int SIZE = 40;
+    private double knobRadius;
+    
+    private final double minAngle = -140;
+    private final double maxAngle = 140;
+    private double dragOffset;
+
+
+    private StackPane knob;
+    private StackPane knobOverlay;
+    private StackPane knobDot;
+
     public Knob() {
-        setSkinClassName(KnobSkin.class.getName());
+        setSkin(new KnobSkin(this));
+        initialize();
     }
+    
+    private void initialize() {
+        knob = new StackPane() {
+            @Override protected void layoutChildren() {
+                knobDot.autosize();
+                knobDot.setLayoutX((knob.getWidth()-knobDot.getWidth())/2);
+                knobDot.setLayoutY(5+(knobDot.getHeight()/2));
+            }
+
+        };
+        knob.getStyleClass().setAll("knob");
+        knobOverlay = new StackPane();
+        knobOverlay.getStyleClass().setAll("knobOverlay");
+        knobDot = new StackPane();
+        knobDot.getStyleClass().setAll("knobDot");
+
+        getChildren().setAll(knob, knobOverlay);
+        knob.getChildren().add(knobDot);
+
+        setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent me) {
+                double dragStart = mouseToValue(me.getX(), me.getY());
+                double zeroOneValue = (getValue() - getMin()) / (getMax() - getMin());
+                dragOffset = zeroOneValue - dragStart;
+                ((KnobSkin) getSkin()).knobPressed(me,dragStart);
+            }
+        });
+        setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent me) {
+                ((KnobSkin) getSkin()).knobRelease(me,mouseToValue(me.getX(), me.getY()));
+            }
+        });
+        setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override public void handle(MouseEvent me) {
+                ((KnobSkin) getSkin()).knobDragged(me, mouseToValue(me.getX(), me.getY()) + dragOffset);
+                rotateKnob();
+            }
+        });
+    }
+
+    private double mouseToValue(double mouseX, double mouseY) {
+        double cx = getWidth()/2;
+        double cy = getHeight()/2;
+        double mouseAngle = Math.toDegrees(Math.atan((mouseY-cy) / (mouseX-cx)));
+        double topZeroAngle;
+        if (mouseX<cx) {
+            topZeroAngle = 90 - mouseAngle;
+        } else {
+            topZeroAngle = -(90 + mouseAngle);
+        }
+        double value = 1 - ((topZeroAngle - minAngle) / (maxAngle - minAngle));
+        return value;
+    }
+
+    void rotateKnob() {
+        double zeroOneValue = (getValue()-getMin()) / (getMax() - getMin());
+        double angle = minAngle + ((maxAngle-minAngle) * zeroOneValue);
+        knob.setRotate(angle);
+    }
+
+    @Override protected void layoutChildren() {
+        // calculate the available space
+        double x = getInsets().getLeft();
+        double y = getInsets().getTop();
+        double w = getWidth() - (getInsets().getLeft() + getInsets().getRight());
+        double h = getHeight() - (getInsets().getTop() + getInsets().getBottom());
+        double cx = x+(w/2);
+        double cy = y+(h/2);
+
+        // resize thumb to preferred size
+        double knobWidth = SIZE; // knob.prefWidth(-1);
+        double knobHeight = SIZE; // knob.prefHeight(-1);
+        knobRadius = Math.max(knobWidth, knobHeight)/2;
+        knob.resize(knobWidth, knobHeight);
+        knob.setLayoutX(cx-knobRadius);
+        knob.setLayoutY(cy-knobRadius);
+        knobOverlay.resize(knobWidth, knobHeight);
+        knobOverlay.setLayoutX(cx-knobRadius);
+        knobOverlay.setLayoutY(cy-knobRadius);
+        rotateKnob();
+    }
+
+    @Override protected double computeMinWidth(double height) {
+        return SIZE; //(getInsets().getLeft() + knob.minWidth(-1) + getInsets().getRight());
+    }
+
+    @Override protected double computeMinHeight(double width) {
+        return SIZE; //(getInsets().getTop() + knob.minHeight(-1) + getInsets().getBottom());
+    }
+
+    @Override protected double computePrefWidth(double height) {
+        return SIZE; //(getInsets().getLeft() + knob.prefWidth(-1) + getInsets().getRight());
+    }
+
+    @Override protected double computePrefHeight(double width) {
+        return SIZE; //(getInsets().getTop() + knob.prefHeight(-1) + getInsets().getBottom());
+    }
+
+    @Override protected double computeMaxWidth(double height) {
+        return Double.MAX_VALUE;
+    }
+
+    @Override protected double computeMaxHeight(double width) {
+        return Double.MAX_VALUE;
+    }
+
 }
--- src/main/java/org/jsynthlib/device/view/KnobSkin.java
+++ src/main/java/org/jsynthlib/device/view/KnobSkin.java
@@ -1,145 +1,102 @@
 package org.jsynthlib.device.view;
 
-import com.sun.javafx.scene.control.skin.SkinBase;
-import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.control.Skin;
 import javafx.scene.control.Slider;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
 
 /**
  * A simple knob skin for slider
  *
  * @author Jasper Potts
  */
-public class KnobSkin extends SkinBase<Slider, KnobBehavior> {
+public class KnobSkin implements Skin<Slider> {
 
-    private static final int SIZE = 40;
-    private double knobRadius;
-    private final double minAngle = -140;
-    private final double maxAngle = 140;
-    private double dragOffset;
-
-    private StackPane knob;
-    private StackPane knobOverlay;
-    private StackPane knobDot;
+    private Slider control;
+    private double dragStartX, dragStartY;
 
     public KnobSkin(Slider slider) {
-        super(slider, new KnobBehavior(slider));
-        initialize();
-        requestLayout();
-        registerChangeListener(slider.minProperty(), "MIN");
-        registerChangeListener(slider.maxProperty(), "MAX");
-        registerChangeListener(slider.valueProperty(), "VALUE");
+        super();
+        this.control = slider;
     }
 
-    private void initialize() {
-        knob = new StackPane() {
-            @Override protected void layoutChildren() {
-                knobDot.autosize();
-                knobDot.setLayoutX((knob.getWidth()-knobDot.getWidth())/2);
-                knobDot.setLayoutY(5+(knobDot.getHeight()/2));
-            }
-
-        };
-        knob.getStyleClass().setAll("knob");
-        knobOverlay = new StackPane();
-        knobOverlay.getStyleClass().setAll("knobOverlay");
-        knobDot = new StackPane();
-        knobDot.getStyleClass().setAll("knobDot");
-
-        getChildren().setAll(knob, knobOverlay);
-        knob.getChildren().add(knobDot);
-
-        getSkinnable().setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent me) {
-                double dragStart = mouseToValue(me.getX(), me.getY());
-                double zeroOneValue = (getSkinnable().getValue() - getSkinnable().getMin()) / (getSkinnable().getMax() - getSkinnable().getMin());
-                dragOffset = zeroOneValue - dragStart;
-                getBehavior().knobPressed(me,dragStart);
-            }
-        });
-        getSkinnable().setOnMouseReleased(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent me) {
-                getBehavior().knobRelease(me,mouseToValue(me.getX(), me.getY()));
-            }
-        });
-        getSkinnable().setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent me) {
-                getBehavior().knobDragged(me, mouseToValue(me.getX(), me.getY()) + dragOffset);
-            }
-        });
-    }
-
-    private double mouseToValue(double mouseX, double mouseY) {
-        double cx = getSkinnable().getWidth()/2;
-        double cy = getSkinnable().getHeight()/2;
-        double mouseAngle = Math.toDegrees(Math.atan((mouseY-cy) / (mouseX-cx)));
-        double topZeroAngle;
-        if (mouseX<cx) {
-            topZeroAngle = 90 - mouseAngle;
-        } else {
-            topZeroAngle = -(90 + mouseAngle);
+    /**
+     * @param position
+     *            The position of mouse in 0=min to 1=max range
+     */
+    public void knobRelease(MouseEvent e, double position) {
+        final Slider slider = getSkinnable();
+        slider.setValueChanging(false);
+        // detect click rather than drag
+        if (Math.abs(e.getX() - dragStartX) < 3
+                && Math.abs(e.getY() - dragStartY) < 3) {
+            slider.adjustValue((position + slider.getMin())
+                    * (slider.getMax() - slider.getMin()));
         }
-        double value = 1 - ((topZeroAngle - minAngle) / (maxAngle - minAngle));
-        return value;
     }
 
-    @Override protected void handleControlPropertyChanged(String p) {
-        super.handleControlPropertyChanged(p);
-        requestLayout();
+    /**
+     * @param position
+     *            The position of mouse in 0=min to 1=max range
+     */
+    public void knobPressed(MouseEvent e, double position) {
+        // If not already focused, request focus
+        final Slider slider = getSkinnable();
+        if (!slider.isFocused()) {
+            slider.requestFocus();
+        }
+        slider.setValueChanging(true);
+        dragStartX = e.getX();
+        dragStartY = e.getY();
     }
 
-    void rotateKnob() {
-        Slider s = getSkinnable();
-        double zeroOneValue = (s.getValue()-s.getMin()) / (s.getMax() - s.getMin());
-        double angle = minAngle + ((maxAngle-minAngle) * zeroOneValue);
-        knob.setRotate(angle);
+    /**
+     * @param position
+     *            The position of mouse in 0=min to 1=max range
+     */
+    public void knobDragged(MouseEvent e, double position) {
+        final Slider slider = getSkinnable();
+        if (slider.getMin() < 0) {
+            slider.adjustValue((position
+                    * (slider.getMax() - slider.getMin())) + slider.getMin());
+        } else {
+            slider.adjustValue((position + slider.getMin())
+                    * (slider.getMax() - slider.getMin()));
+        }
     }
 
-    @Override protected void layoutChildren() {
-        // calculate the available space
-        double x = getInsets().getLeft();
-        double y = getInsets().getTop();
-        double w = getWidth() - (getInsets().getLeft() + getInsets().getRight());
-        double h = getHeight() - (getInsets().getTop() + getInsets().getBottom());
-        double cx = x+(w/2);
-        double cy = y+(h/2);
-
-        // resize thumb to preferred size
-        double knobWidth = SIZE; // knob.prefWidth(-1);
-        double knobHeight = SIZE; // knob.prefHeight(-1);
-        knobRadius = Math.max(knobWidth, knobHeight)/2;
-        knob.resize(knobWidth, knobHeight);
-        knob.setLayoutX(cx-knobRadius);
-        knob.setLayoutY(cy-knobRadius);
-        knobOverlay.resize(knobWidth, knobHeight);
-        knobOverlay.setLayoutX(cx-knobRadius);
-        knobOverlay.setLayoutY(cy-knobRadius);
-        rotateKnob();
+    void home() {
+        final Slider slider = getSkinnable();
+        slider.adjustValue(slider.getMin());
     }
 
-    @Override protected double computeMinWidth(double height) {
-        return SIZE; //(getInsets().getLeft() + knob.minWidth(-1) + getInsets().getRight());
+    void decrementValue() {
+        getSkinnable().decrement();
     }
 
-    @Override protected double computeMinHeight(double width) {
-        return SIZE; //(getInsets().getTop() + knob.minHeight(-1) + getInsets().getBottom());
+    void end() {
+        final Slider slider = getSkinnable();
+        slider.adjustValue(slider.getMax());
     }
 
-    @Override protected double computePrefWidth(double height) {
-        return SIZE; //(getInsets().getLeft() + knob.prefWidth(-1) + getInsets().getRight());
+    void incrementValue() {
+        getSkinnable().increment();
     }
 
-    @Override protected double computePrefHeight(double width) {
-        return SIZE; //(getInsets().getTop() + knob.prefHeight(-1) + getInsets().getBottom());
+    @Override
+    public Slider getSkinnable() {
+        return control;
     }
 
-    @Override protected double computeMaxWidth(double height) {
-        return Double.MAX_VALUE;
+    @Override
+    public Node getNode() {
+        return null;
     }
 
-    @Override protected double computeMaxHeight(double width) {
-        return Double.MAX_VALUE;
+    @Override
+    public void dispose() {
+        control = null;
     }
+
 }
 
